23d9b1603825025d5f53c2450a2576c53a26d0ee
Shakil Shahadat <shakil.regi@gmail.com>
2020-03-26 20:29:30 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>News</title>
+	<link rel="stylesheet" href="src/style.css">
+</head>
+<body>
+
+<main>
+	<h3>বাংলা</h3>
+	<ol>
+		<li>
+			<a href="http://www.amadershomoy2.com/">আমাদের সময়</a>
+		</li>
+		<li>
+			<a href="http://www.amaderorthoneeti.com/">আমাদের অর্থনীতি</a>
+		</li>
+		<li>
+			<a href="http://www.ittefaq.com.bd/">ইত্তেফাক</a>
+		</li>
+		<li>
+			<a href="http://www.dailyinqilab.com/">ইনকিলাব</a> |
+			<a href="http://www.einqilab.com/">ই-পেপার</a>
+		</li>
+		<li>
+			<a href="http://www.kalerkantho.com/">কালের কণ্ঠ</a> |
+			<a href="http://www.ekalerkantho.com/">ই-পেপার</a> |
+			<a href="http://www.kalerkantho.com/print-edition/tech-everyday">টেক প্রতিদিন</a>
+		</li>
+		<li>
+			<a href="http://www.natunbarta.com/">নতুন বার্তা</a>
+		</li>
+		<li>
+			<a href="http://www.dailynayadiganta.com/">নয়াদিগন্ত</a>
+		</li>
+		<li>
+			<a href="http://www.dainikpurbokone.net">পূর্বকোণ</a>
+		</li>
+		<li>
+			<a href="http://www.prothom-alo.com/">প্রথম আলো</a> |
+			<a href="http://www.prothom-alo.com/tp_tech/">কম্পিউটার প্রতিদিন</a> |
+			<a href="http://www.prothom-alo.com/technology">বিজ্ঞান ও প্রযুক্তি</a> |
+			<a href="http://www.prothom-alo.com/life_style">জীবনযাপন</a> |
+			<a href="http://www.prothom-alo.com/tp_world/">সারা বিশ্ব</a>
+		</li>
+		<li>
+			<a href="http://www.bhorerkagoj.net/new/">ভোরের কাগজ</a>
+		</li>
+		<li>
+			<a href="http://www.bd-pratidin.com/">বাংলাদেশ প্রতিদিন</a>
+		</li>
+		<li>
+			<a href="http://www1.bssnews.net/bangla/">বাংলাদেশ সংবাদ সংস্থা</a>
+		</li>
+		<li>
+			<a href="http://www.banglanews24.com/">বাংলা নিউজ টোয়েন্টিফোর</a>
+		</li>
+		<li>
+			<a href="http://bangla.bdnews24.com/">বিডি নিউজ টুয়েন্টিফোর</a> |
+			<a href="http://bangla.bdnews24.com/tech/">টেক</a>
+		</li>
+		<li>
+			<a href="http://www.bbc.co.uk/bengali/">বিবিসি বাংলা</a>
+		</li>
+		<li>
+			<a href="http://www.mzamin.com/">মানবজমিন</a>
+		</li>
+		<li>
+			<a href="http://www.jjdin.com/">যায় যায় দিন</a>
+		</li>
+		<li>
+			<a href="http://www.jugantor.com/">যুগান্তর</a> |
+			<a href="http://www.ejugantor.com/">ই-যুগান্তর</a> |
+			<a href="http://www.jugantor.com/it-technology">আইটি ও প্রযুক্তি</a> |
+			<a href="http://www.jugantor.com/ten-horizon">দশ দিগন্ত</a>
+		</li>
+	 	<li>
+	 		<a href="http://www.risingbd24.com/">রাইজিং বাংলাদেশ</a>
+	 	</li>
+		<li>
+			<a href="http://www.shokalerkhabor.com/">সকালের খবর</a>
+		</li>
+		<li>
+			<a href="http://www.samakal.com.bd/">সমকাল</a>
+		</li>
+		<li>
+			<a href="http://www.thedailysangbad.com/">সংবাদ</a>
+		</li>
+		<li>
+			<a href="http://www.shongbadprotidin.com/">সংবাদ প্রতিদিন</a>
+		</li>
+		<li>
+			<a href="http://www.sheershanews.com/">শীর্ষ নিউজ</a>
+		</li>
+	</ol>
+
+	<h3>English</h3>
+	<ol>
+		<li>
+			<a href="http://bdnews24.com/">BD News 24</a>
+		</li>
+		<li>
+			<a href="http://www1.bssnews.net/">Bangladesh Sangbad Sangstha (BSS)</a>
+		</li>
+		<li>
+			<a href="http://www.dhakatribune.com/">Dhaka Tribune</a>
+		</li>
+		<li>
+			<a href="http://www.thedailystar.net/">The Daily Star</a>
+		</li>
+		<li>
+			<a href="http://www.timesofassam.com/">Times of Assam</a>
+		</li>
+	</ol>
+
+	<h3>TV</h3>
+	<ol>
+	 	<li>
+	 		<a href="http://www.ekushey-tv.com/">Ekushey Television</a>
+	 	</li>
+		<li>
+			<a href="http://independent24.tv/">Independent24</a>
+		</li>
+		<li>
+			<a href="https://www.ntvbd.com/">NTV</a>
+		</li>
+	 	<li>
+	 		<a href="http://www.somoynews.tv/">Somoy TV</a>
+	 	</li>
+	</ol>
+
+	<h3>Newspaper Links Collection</h3>
+	<ol>
+		<li><a href="http://bdrob.com/">bdrob.com</a></li>
+	</ol>
+</main>
+
+<script>
+// Add target="_blank" to all anchor tags, v 1.04
+for ( let i = 0; i < document.querySelectorAll( 'a' ).length; i++ )
+{
+	document.querySelectorAll( 'a' )[ i ].setAttribute( 'target', '_blank' );
+}
+</script>
+</body>
+</html>

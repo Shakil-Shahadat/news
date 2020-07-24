ace8510c07c97a2f956256dc23f5500e3108266b
Shakil Shahadat <shakil.regi@gmail.com>
2020-07-24 14:17:13 +0600

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		<li>
 			<a href="https://www.kalerkantho.com/">কালের কণ্ঠ</a> |
 			<a href="http://www.ekalerkantho.com/">ই</a> |
-			<a href="http://www.kalerkantho.com/print-edition/tech-everyday">টেক প্রতিদিন</a>
+			<a href="https://www.kalerkantho.com/print-edition/tech-everyday">টেক প্রতিদিন</a>
 		</li>
 		<li>
 			<a href="http://www.dailynayadiganta.com/">নয়াদিগন্ত</a>

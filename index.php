d9373aba91490e78bb6745a3818ff39dbacb6bbb
Shakil Shahadat <shakil.regi@gmail.com>
2020-09-18 18:19:53 +0600

    Update index.php

--- index.php
+++ index.php
@@ -103,7 +103,6 @@
 			<a href="http://www.dainikpurbokone.net">পূর্বকোণ</a>
 		</li>
 		<li>
-			<a href="https://www.bhorerkagoj.com/">ভোরের কাগজ</a>
 			<a href="http://www.bssnews.net/bangla/">বাংলাদেশ সংবাদ সংস্থা</a>
 		</li>
 		<li>

137c7f56f233305a16ead5a5890bb6c92839d616
Shakil Shahadat <shakil.regi@gmail.com>
2021-10-24 06:55:11 +0600

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,6 @@
 	<h3>বাংলা ( গুরুত্বপূর্ন )</h3>
 	<ol>
 		<li>
-			<a href="https://amardesh.co.uk/">আমার দেশ</a>
-		</li>
-		<li>
 			<a href="http://www.ittefaq.com.bd/">ইত্তেফাক</a> |
 			<a href="https://www.ittefaq.com.bd/print-edition">আজকের পত্রিকা</a> |
 			<a href="https://epaper.ittefaq.com.bd/">ই</a>

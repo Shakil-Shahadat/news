89c8ae0053cddc35ed81b95c879ca248d79402bb
Shakil Shahadat <shakil.regi@gmail.com>
2022-03-01 14:40:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 		<li>
 			<a href="https://www.kalerkantho.com/">কালের কণ্ঠ</a> |
 			<a href="http://www.ekalerkantho.com/">ই</a> |
+			<a href="https://www.kalerkantho.com/print-edition/">আজকের পত্রিকা</a>
 			<a href="https://www.kalerkantho.com/online/info-tech">তথ্যপ্রযুক্তি</a> |
 			<a href="https://www.facebook.com/kalerkantho">FB</a>
 		</li>

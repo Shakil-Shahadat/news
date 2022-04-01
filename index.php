e15235af2fabc721d2df73cef65005be09c7ccb1
Shakil Shahadat <shakil.regi@gmail.com>
2022-04-01 17:54:50 +0600

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 			<a href="https://www.facebook.com/BanglaTribuneOnline">FB</a>
 		</li>
 		<li>
-			<a href="http://www.bd-pratidin.com/">বাংলাদেশ প্রতিদিন</a> |
+			<a href="https://www.bd-pratidin.com/">বাংলাদেশ প্রতিদিন</a> |
 			<a href="https://www.bd-pratidin.com/printversion">আজকের পত্রিকা</a> |
 			<a href="http://www.ebdpratidin.com/">ই</a> |
 			<a href="https://www.bd-pratidin.com/tech-world">টেক ওয়ার্ল্ড</a>

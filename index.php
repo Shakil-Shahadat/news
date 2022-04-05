b887abff27f1d95de7b30babb1feaa5da0617156
Shakil Shahadat <shakil.regi@gmail.com>
2022-04-05 11:26:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -129,7 +129,8 @@
 			<a href="http://www.bssnews.net/bangla/">বাংলাদেশ সংবাদ সংস্থা</a>
 		</li>
 		<li>
-			<a href="https://www.bhorerkagoj.com/">ভোরের কাগজ</a>
+			<a href="https://www.bhorerkagoj.com/">ভোরের কাগজ</a> |
+			<a href="https://www.bhorerkagoj.com/print-edition/">আজকের পত্রিকা</a> |
 		</li>
 		<li>
 			<a href="http://www.shershanews24.com/">শীর্ষ নিউজ</a>

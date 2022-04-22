099ffe3e8c6e17fe854bd47864166a82cc806ee0
Shakil Shahadat <shakil.regi@gmail.com>
2022-04-23 00:57:26 +0600

    Update index.php

--- index.php
+++ index.php
@@ -100,6 +100,7 @@
 		<li>
 			<a href="https://www.jugantor.com/">যুগান্তর</a> |
 			<a href="https://epaper.jugantor.com/">ই</a> |
+			<a href="https://www.jugantor.com/todays-paper">আজকের পত্রিকা</a> |
 			<a href="https://www.jugantor.com/todays-paper/it-world">আইটি বিশ্ব</a> |
 			<a href="https://www.jugantor.com/todays-paper/ten-horizon">দশ দিগন্ত</a>
 		</li>

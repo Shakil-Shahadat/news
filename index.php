e5221f247f60bca0d81e24bfb292ff5d5ddabe40
Shakil Shahadat <shakil.regi@gmail.com>
2022-04-23 00:24:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -89,6 +89,7 @@
 		<li>
 			<a href="https://www.jaijaidinbd.com/">যায় যায় দিন</a> |
 			<a href="https://www.jaijaidinbd.com/todays-paper">আজকের পত্রিকা</a> |
+			<a href="https://www.jaijaidinbd.com/international">আন্তর্জাতিক</a> |
 		</li>
 		<li>
 			<a href="https://www.jugantor.com/">যুগান্তর</a> |

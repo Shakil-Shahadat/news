97b2086f1ddfbd217cc41a8aa2c35a4450a1b905
Shakil Shahadat <shakil.regi@gmail.com>
2022-05-23 06:25:44 +0600

    Update index.php

--- index.php
+++ index.php
@@ -43,6 +43,7 @@
 			<a href="https://www.prothomalo.com/">প্রথম আলো</a> |
 			<a href="https://epaper.prothomalo.com/">ই</a> |
 			<a href="https://www.prothomalo.com/world/">সারা বিশ্ব</a> |
+			<a href="https://www.prothomalo.com/business">অর্থনীতি</a> |
 			<a href="https://www.prothomalo.com/education/science-tech">প্রযুক্তি</a> |
 			<a href="https://www.prothomalo.com/lifestyle">লাইফস্টাইল</a> |
 			<a href="https://www.youtube.com/c/ProthomAlo/videos">YT</a> |

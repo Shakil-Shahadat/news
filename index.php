dccdb6a149dad7a7af887995070b1937a048bbaf
Shakil Shahadat <shakil.regi@gmail.com>
2022-06-10 13:28:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -416,9 +416,7 @@
 		</li>
 		<li>
 			<a href="https://www.thehindu.com/">The Hindu</a> |
-			<a href="https://www.youtube.com/c/TheHinduOfficial/videos">
-				The Hindu
-			</a>
+			<a href="https://www.youtube.com/c/TheHinduOfficial/videos">YT</a>
 		</li>
 		<li>
 			<a href="https://thewire.in/">The Wire</a>

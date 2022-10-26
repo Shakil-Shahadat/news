c36730d0ea98ca05524d5df96a39e9297363b18f
Shakil Shahadat <shakil.regi@gmail.com>
2022-10-27 01:37:19 +0600

    Update index.php

--- index.php
+++ index.php
@@ -274,7 +274,7 @@
 		<li>
 			<a href="https://www.channel24bd.tv/">Channel 24</a> |
 			<a href="https://www.youtube.com/c/Channel24Bulletin/videos">YT 1</a> |
-			<a href="https://www.youtube.com/c/channel24digital/videos">YT</a> |
+			<a href="https://www.youtube.com/c/channel24digital/videos">YT 2</a> |
 			<a href="https://www.channel24bd.tv/live">Live</a> |
 			<a href="https://www.facebook.com/channel24live">FB</a>
 		</li>

51d851b032ec0d50bd1a84b58719be1c8a0bf10a
Shakil Shahadat <shakil.regi@gmail.com>
2022-10-26 22:22:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -264,7 +264,7 @@
 			<a href="https://www.facebook.com/BoishakhiTvNews">FB</a>
 		</li>
 		<li>
-			<a href="https://www.channelionline.com/">Channel I.com</a> |
+			<a href="https://www.channelionline.com/">Channel i</a> |
 			<a href="https://www.youtube.com/channel/UC8NcXMG3A3f2aFQyGTpSNww/videos">News YT</a> |
 			<a href="https://www.youtube.com/channel/UCLuPUjbBSIYmltR5D8Srq7A/videos">Shows</a> |
 			<a href="https://www.youtube.com/channel/UCfNBi-m7X8ntCBbban1QaYw/videos">Multimedia</a> |

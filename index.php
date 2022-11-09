8e2aaf025757c752cd6ea582e21b1bbdf73d783d
Shakil Shahadat <shakil.regi@gmail.com>
2022-11-10 03:47:40 +0600

    Update index.php

--- index.php
+++ index.php
@@ -298,6 +298,7 @@
 	 		<a href="https://www.ekushey-tv.com/">Ekushey Television</a> |
 	 		<a href="https://www.youtube.com/c/ETVNewsvideo/videos">YT 1</a> |
 	 		<a href="https://www.youtube.com/c/EkusheyETV/videos">YT 2</a> |
+	 		<a href="https://www.youtube.com/c/EtvTalkShow/videos">Talk Show</a> |
 	 		<a href="https://www.ekushey-tv.com/etvlive/">Live</a> |
 	 		<a href="https://www.facebook.com/Ekushey24online/">FB</a>
 	 	</li>

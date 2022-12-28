55e88cd9a48f34a16e3488c66fbde46d72792ff7
Shakil Shahadat <shakil.regi@gmail.com>
2022-12-29 00:56:24 +0600

    Update index.php

--- index.php
+++ index.php
@@ -239,7 +239,7 @@
 	<ol>
 		<li>
 			<!-- Website Down -->
-			<a href="https://www.youtube.com/c/ATNBanglanews/videos">ATN Bangla News YT</a> |
+			<a href="https://www.youtube.com/@ATNBanglanews/videos">ATN Bangla News YT</a> |
 			<a href="https://www.youtube.com/c/ATNBanglaTalkShow/videos">Talk Show</a> |
 			<a href="https://www.youtube.com/c/ATNBanglaIslamic/videos">Islamic</a> |
 			<a href="https://www.facebook.com/atnbanglanewsofficial">FB</a>

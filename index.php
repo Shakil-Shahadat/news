1a101d5c1decad6ce084cb9bda25ce24b662efb9
Shakil Shahadat <shakil.regi@gmail.com>
2023-06-25 22:03:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 			<a href="https://www.kalerkantho.com/online/business">বাণিজ্য</a> |
 			<a href="https://www.kalerkantho.com/print-edition/sub-editorial">মতামত</a> |
 			<a href="https://www.kalerkantho.com/online/info-tech">তথ্যপ্রযুক্তি</a> |
+			<a href="https://www.kalerkantho.com/online/Islamic-lifestylie">ইসলাম</a> |
 			<a href="https://www.kalerkantho.com/feature/doctor-asen">স্বাস্থ্য</a> |
 			<a href="https://www.kalerkantho.com/english">Eng</a> |
 			<a href="https://www.facebook.com/kalerkantho">FB</a>

6b927e15c7375c9afea004bc06e554b48ce51cca
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-07 18:22:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -705,8 +705,8 @@
 
 <script>
 
-	// Add target="_blank" to all anchor tags, v 1.05
-	for ( x of document.querySelectorAll( 'a' ) ) x.setAttribute( 'target', '_blank' );
+	// Add target="_blank" to all anchor tags, v 2.0
+	for ( x of document.links ) x.setAttribute( 'target', '_blank' );
 
 	for ( e of document.querySelectorAll( 'li' ) )
 	{
